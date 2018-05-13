10d8e99d7935aade9ab6381617a7f1deed56d4d2
Zengs <zingsono@gmail.com>
2018-05-13 14:31:09 +0800

    docker-houri:1.0-centos7

--- dockerfile/docker-houri/centos7/1.0/docker.js
+++ dockerfile/docker-houri/centos7/1.0/docker.js
@@ -43,5 +43,5 @@ const server = http.createServer((req, res) => {
 server.on('clientError', (err, socket) => {
     socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
-server.listen(8000);
+server.listen(80);
 
---
+++ dockerfile/docker-houri/centos7/README.md
@@ -0,0 +1,8 @@
+FROM centos:7
+===================
+
+
+宿主机是Centos7,构建Centos7镜像，执行宿主机docker命令。
+
+
+
