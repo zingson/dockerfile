ee7d139243361536202c0cef2c7953de2525d640
Zengs <zingsono@gmail.com>
2018-06-21 02:24:49 +0800

    zingsono/jenkins

--- zingsono/jenkins/scloud/docker-service.sh
+++ zingsono/jenkins/scloud/docker-service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Jenkins 自动部署 Spring boot 服务，当前脚本需要在jenkins容器中使用gradle触发执行
+# /var/jenkins_home/bin/docker-service.sh
 
 # 参数： 服务名、版本号
 S_NAME=$1
@@ -24,12 +25,14 @@ ARGS="sh -c 'curl -s -O $JAR_URL && $JAR_BOOT'"
 
 # 判断服务是否存在，存在更新镜像，不存在创建
 SID=$(docker service ls -qf name=$S_NAME)
-if [ ""x == "$SID"x ] ; then
-    echo "> Service Create: docker service create --name $S_NAME --network cluster --replicas 1 -d zingsono/openjdk:1.8-jre $ARGS"
-    docker service create --name $S_NAME --network cluster --replicas 1 -d zingsono/openjdk:1.8-jre $ARGS
+if [ "x" == "x$SID" ] ; then
+    DOCKER_CREATE="docker service create --name $S_NAME --network cluster --replicas 1 -d zingsono/openjdk:8-jre $ARGS"
+    echo "> $DOCKER_CREATE"
+    eval $DOCKER_CREATE
 else
-    echo "> Service Update: docker service update --force --update-parallelism 1 --update-delay 120s --args \"$ARGS\" $S_NAME"
-    docker service update --force --update-parallelism 1 --update-delay 120s --args "$ARGS" $S_NAME
+    DOCKER_UPDATE="docker service update --force --update-parallelism 1 --update-delay 120s --args \"$ARGS\" $S_NAME"
+    echo "> $DOCKER_UPDATE"
+    eval $DOCKER_UPDATE
 fi
 
 echo " ^_^ *  ^_^ "
