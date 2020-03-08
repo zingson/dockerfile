03ed560f6c350827967a951e1b4de58cdf2de8cb
zengs <zengs@unionlive.com>
2020-03-08 22:54:13 +0800

    php fpm-nginx

--- mszs/php/7.4-fpm-nginx-alpine/entrypoint.sh
+++ mszs/php/7.4-fpm-nginx-alpine/entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-if [ $GIT_URL ] then
-  if [ $GIT_TAG ] then
-    git clone -b $GIT_TAG &GIT_URL
-  else
-    git clone &GIT_URL
+# 环境变量参数
+#GIT_URL=https://zingson:******@gitee.com/wangxie_139/rui_dian_project.git
+#GIT_TAG=v1.0.1
+
+_substr=${GIT_URL##*/}
+pname=${_substr%.git}
+echo "pname : $pname"
+if [ $GIT_URL ]; then
+  echo "#>git clone $GIT_URL"
+  git clone $GIT_URL
+  if [ $GIT_TAG ]; then
+    cd $pname
+    echo "#>git checkout -b $GIT_TAG"
+    git checkout -b $GIT_TAG
   fi
 fi
 
+# Nginx配置文件
 cat > /etc/nginx/nginx.conf << EOF
 user nginx;
 worker_processes auto;
