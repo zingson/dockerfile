8a7347a7321b8413e028958c6b8804ce3639df07
zengs <zengs@unionlive.com>
2020-03-10 10:33:26 +0800

    php fpm-nginx

--- mszs/php/7.4-fpm-nginx-alpine/entrypoint.sh
+++ mszs/php/7.4-fpm-nginx-alpine/entrypoint.sh
@@ -17,39 +17,5 @@ if [ $GIT_URL ]; then
   fi
 fi
 
-# Nginx配置文件
-cat > /etc/nginx/nginx.conf << EOF
-user nginx;
-worker_processes auto;
-pid       /var/run/nginx.pid;
-events {
-    worker_connections  1024;
-}
-http {
-  include /etc/nginx/mime.types;
-  default_type application/octet-stream;
-  client_max_body_size 10m;
-  keepalive_timeout 65;
-  server {
-    listen       80;
-    server_name  localhost;
-    root         /var/www/html/;
-
-    #charset koi8-r;
-    #access_log  /var/log/nginx/host.access.log  main;
-
-    location / {
-        index  index.html index.htm index.php;
-    }
-    location ~ \.php$ {
-        fastcgi_pass   127.0.0.1:9000;
-        fastcgi_index  index.php;
-        include        fastcgi_params;
-        fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
-    }
-  }
-}
-EOF
-
 nginx
 php-fpm
